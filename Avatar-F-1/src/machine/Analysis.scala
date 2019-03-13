5cb099651ba8aefefa205aa8cd127121254ab6ee
xstun <xstun@10.41.6.156>
2019-03-13 16:51:01 +0100

    analysis 13/03

--- Avatar-F-1/src/machine/Analysis.scala
+++ Avatar-F-1/src/machine/Analysis.scala
@@ -11,10 +11,14 @@ object Analysis
     val lieux = List("gare","théâtre","mairie","hôtel","ville","rennes","bretagne","sncf","tnb","paillette","national","place")
     
     val mairie = List("marie","hotel","ville","place","rennes")
+    var m = 0 //
     val paillete = List("paillete","théâtre")
+    var p = 0
     val TNB = List("théâtre","national","tnb","bretagne")
+    var t = 0
     val gare = List("gare","sncf")
-    
+    var g = 0
+    val phrases = List()
     
     
     
@@ -26,14 +30,33 @@ object Analysis
     val l = verbe::lieux 
     
     
-    def addtokeyword(l:List[Char]) = {
+    def addtokeyword(r: Request) = {
       var a = r.rawInput.split(" ")
       for (i <- a){
         if(lieux.contains(i)){
           r.keywords = r.keywords:+i
         }
       }
-      
+    }
+    
+    
+    def checkkeyword(s: List[String]): Unit = {
+      for(x <- s) {   
+      if (mairie.contains(x)) m=m+1
+      if (paillete.contains(x)) p=p+1
+      if (TNB.contains(x))t=t+1
+      if (gare.contains(x))g=g+1 
+      }
+    }
+    
+    
+    def isValide(r: Request) = {
+      addtokeyword(r)
+      if (r.keywords.isEmpty) r.valid = false
+      else {
+        
+        
+      }
     }
     
     
