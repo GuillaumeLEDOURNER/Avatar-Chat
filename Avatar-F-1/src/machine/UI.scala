0c8f629939a385e5b0d42c82a492b1aff0980814
Quemerais Primael <primael.quemerais@etudiant.univ-rennes1.fr>
2019-03-14 09:46:04 +0100

    Commit UI.

--- Avatar-F-1/src/machine/UI.scala
+++ Avatar-F-1/src/machine/UI.scala
@@ -2,6 +2,7 @@ package machine
 
 import scala.collection.mutable.ListBuffer
 import java.awt.Dimension
+import scala.util.Random
 
 import scala.swing._
 import scala.swing.event._
@@ -11,10 +12,12 @@ case class SomethingHappened() extends Event {
 }
 
 object UI {
- 
-  var rowData = Array("Bonjour que recherchez vous dans cet interface")
    
-  var buffer = ListBuffer("Avatar : Bonjour que recherchez vous ?")
+  var buffer = ListBuffer("<html><font color='white'>Avatar : Wesh !</font></html>")
+  
+  var questions = Array("Une autre question ?", "Cherchez vous autre chose ?", "Que puis-je faire d'autre pour vous ?", "Très bonne question ! Autre chose ?", "")
+  
+  val randomizer = new Random
   
   def init() {
     val frame = new MainFrame {
@@ -26,31 +29,38 @@ object UI {
         listData = buffer
         fixedCellWidth = 450
         fixedCellHeight = 20
-        preferredSize = new Dimension(480, 400)
+        background = new Color(255, 255, 255);
       }
       
-      val inputField = new TextField 
-      {
+      val inputField = new TextField {
         maximumSize = new Dimension(400, 100)
       }
      
       val sendButton = new Button {
-        text = "Send"
-      reactions += {
-        case ButtonClicked(_) => {
-          this.publish(SomethingHappened())
-            list.listData = buffer += "Moi : " + inputField.text
+        text = "<html><font color='red'>Send</font></html>"
+        maximumSize = new Dimension(100, 20)
+        background = new Color(255, 255, 255);
+        reactions += {
+          case ButtonClicked(_) => {
+            this.publish(SomethingHappened())
+            list.listData = buffer += "<html><font color='black'>Moi : " + inputField.text + "</font></html>"
+            inputField.text = ""
+            list.listData = buffer += "<html><font color='black'>Avatar : " + questions(randomizer.nextInt(questions.size)) + "</font></html>"
           }
         }
       }
      
       contents = new BoxPanel(Orientation.Vertical) {
+        background = new Color(255, 255, 255);
         contents += new FlowPanel() {
+          background = new Color(255, 255, 255);
           contents += new ScrollPane(list) {
-            preferredSize = new Dimension(500, 400)
+            background = new Color(255, 255, 255);
+            preferredSize = new Dimension(500, 350)
           }
         }
         contents += new BoxPanel(Orientation.Horizontal) {
+          background = new Color(255, 255, 255);
           contents += inputField
           contents += sendButton
         }
