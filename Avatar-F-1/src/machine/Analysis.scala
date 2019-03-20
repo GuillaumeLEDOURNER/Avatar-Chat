69d4893f65f2e4655e43af5e6bd98a6ac55ad650
Skinz <Skinz@DESKTOP-I9DS839>
2019-03-20 15:48:31 +0100

    Analysis words kind

--- Avatar-F-1/src/machine/Analysis.scala
+++ Avatar-F-1/src/machine/Analysis.scala
@@ -5,6 +5,10 @@ import java.util.Arrays
 object Analysis 
 {
     val searchKeywords = List("chercher", "cherche", "aller", "trouver", "recherche")
+    
+    val wordsKind = List[(String,String)](("F","Mairie"),("M","Théâtre"),("F","Gare"))
+    
+    
     var hasSearch = false
     
     def addtokeyword(r: Request) = 
@@ -13,14 +17,27 @@ object Analysis
     }
     
     
-    def checkkeyword(s: List[String]): Unit =
+    def getDeterminant(s: String): String =
     {
-    
-      
+         for(kind <- wordsKind)
+         {
+            if (kind._2.toLowerCase() == s.toLowerCase())
+            {
+             
+                return kind._1 match
+                {
+                  case "F" => "La"
+                  case "M" => "Le"
+                }
+            }
+         }
+         ""
+         
     }
     
     def analyse(r: Request): Unit = 
     {
+      
       hasSearch = false
       println(r.toString)
       val words = r.rawInput.split(" ")
@@ -36,15 +53,17 @@ object Analysis
         val keys = Data.getKeys()
         for(k <- keys) {
           for(w <- words) {
-            println("kword : " + k)
+           
             val keywords = k.split(" ")
+            r.keywords = keywords.toList;
+            
             for(kw <- keywords) {
               if(kw.length > 2) {
-                println("kword splitted filtered : " + kw)
-                println("Checking " + w + " with " + kw)
-                if(MyCorrection.distancedeHamming(w, kw)) {
-                  println("Found : " + Data.getValue(k))
-                  r.results ::= Data.getValue(k)
+             
+                if(MyCorrection.distancedeHamming(w, kw)) 
+                {
+                  val det=  getDeterminant(w)
+                  r.results ::= det +" "+ w +" se trouve "+ Data.getValue(k)
                   return
                 }
               }
