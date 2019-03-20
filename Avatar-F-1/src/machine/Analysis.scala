8cadc9658b4a1780c0e9d40580f82403e983efa2
Quemerais Primael <primael.quemerais@etudiant.univ-rennes1.fr>
2019-03-20 17:46:29 +0100

    Updated

--- Avatar-F-1/src/machine/Analysis.scala
+++ Avatar-F-1/src/machine/Analysis.scala
@@ -4,52 +4,16 @@ import java.util.Arrays
 
 object Analysis 
 {
-    //val searchKeywords = List("chercher", "cherche", "aller", "trouver", "recherche", "ou")
-    
-    val wordsKind = List[(String,String)](("F","Mairie"),("M","Théâtre"),("F","Gare"))
-    
-    var hasSearch = false
-    
     def addtokeyword(r: Request) = 
     {
      
     }
     
-    
-    def getDeterminant(s: String): String =
-    {
-         for(kind <- wordsKind)
-         {
-            if (kind._2.toLowerCase() == s.toLowerCase())
-            {
-             
-                return kind._1 match
-                {
-                  case "F" => "La"
-                  case "M" => "Le"
-                }
-            }
-         }
-         ""
-    }
-    
     def analyse(r: Request): Unit = 
     {
-      
-      hasSearch = true
-      /*println(r.toString)
       val words = r.rawInput.split(" ")
       for(w <- words) {
-        for(s <- searchKeywords) {
-          if(MyCorrection.distancedeHamming(w, s)) {
-            println( w + " == " + s)
-            hasSearch = true
-          }
-        }
-      }*/
-      val words = r.rawInput.split(" ")
-      for(w <- words) {
-        if(MyCorrection.distancedeHamming(w, "bonjour")) 
+        if(MyCorrection.distancedeHamming(w, "bonjour") || MyCorrection.distancedeHamming(w, "salut")) 
         {
           r.results ::= "Bonjour"
           if(words.length == 1) {
@@ -57,18 +21,17 @@ object Analysis
           }
         }
       }
-      if(hasSearch) {
-        val keys = Data.getKeys()
-        for(k <- keys) {
-          for(w <- words) {
-            val keywords = k.split(" ")
-            r.keywords = keywords.toList;
-            for(kw <- keywords) {
-              if(kw.length > 2) {
-                println("Checking : " + w + " with " + kw)
+      val keys = Data.getKeys()
+      for(k <- keys) {
+        for(w <- words) {
+          val keywords = k.split(" ")
+          r.keywords = keywords.toList;
+          for(kw <- keywords) {
+            if(kw.length > 2) {
+              println("Checking : " + w + " with " + kw)
+              if(!MyCorrection.distancedeHamming(w, "theatre")) {
                 if(MyCorrection.distancedeHamming(w, kw)) 
                 {
-                  //val det = getDeterminant(w)
                   r.results ::= "L'adresse de " + Data.getName(k) +" est : "+ Data.getValue(k)
                   println(r.results)
                   return
@@ -77,8 +40,6 @@ object Analysis
             }
           }
         }
-      } else {
-        println("No search keyword found")
       }
       r.results ::= "Je ne comprends pas votre demande"
       println(r.results)
