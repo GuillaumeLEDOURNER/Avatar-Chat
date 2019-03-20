5d4cc7248e7ee682857471533a5b7e157a80a03c
Quemerais Primael <primael.quemerais@etudiant.univ-rennes1.fr>
2019-03-20 17:10:02 +0100

    Client

--- Avatar-F-1/src/client/Client.scala
+++ Avatar-F-1/src/client/Client.scala
@@ -4,17 +4,11 @@ import machine._
 import automaticTester.TestAvatar
 
 object Client extends App {
-<<<<<<< HEAD
-  UI.init()
-=======
+
 	  UI.init()
->>>>>>> branch 'master' of https://gitlab.istic.univ-rennes1.fr/tcharrie/avatar-2018-2019-f-1.git
+
 	Data.openFile()
-	
 	TestAvatar.check(MachineImpl)
-<<<<<<< HEAD
-=======
 	
 
->>>>>>> branch 'master' of https://gitlab.istic.univ-rennes1.fr/tcharrie/avatar-2018-2019-f-1.git
 }
