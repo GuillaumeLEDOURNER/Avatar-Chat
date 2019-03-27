fccefa8d09cb578c59a202806f516db9dde99ebd
18000744 <18000744@d122m06.istic.univ-rennes1.fr>
2019-03-27 17:56:22 +0100

    pour pull

--- Avatar-F-1/src/machine/Possibilities.scala
+++ Avatar-F-1/src/machine/Possibilities.scala
@@ -1,44 +1,61 @@
 package machine
 
+
+
 object Possibilities {
-    var plusieurs = false
-    var nbReponses = 0
-    var listReponses : List[String] = List()
+  var plusieurs = false
+  var nbReponses = 0
+  var listReponses : List[String] = List()
+  var recherche = ""
+  
+  val L : List[String] = List("je","de", "une")
+  XMLSearch.init()
+  
   def recherche(r :Request){
-   
-    
     val words = r.rawInput.split(" ") 
     val keys = Data.getKeys()
+    
     for(k <- keys) { //tous les mots clés de Data
     	for(w <- words) { //tous les mots de la requète
+    	  
     		if(w.equals("un") || w.equals("une")){
     			plusieurs = true
     		}
+    		
     		if(plusieurs){
     			val keywords = k.split(" ")
-    			r.keywords = keywords.toList
-    			if(MyCorrection.distancedeHamming(k, w)){
-    			  nbReponses +=1
+    			for(kw <- keywords){
+    				if(MyCorrection.distancedeHamming(w, kw)){
+    					nbReponses +=1
+    				}
     			}
-    			
     		}
+    		println(nbReponses)
     		var i = 0
+    		
     		while(nbReponses != 0 && i<=nbReponses){
-    		  if(i == 0){
+    		  
+    		  if(i == 0)
+    		  {
     		    r.results ::= "J'ai " + nbReponses + " réponses possibles :"
     		    i=i+1
-    		  } else {
-    		  listReponses ::= i + ") " +Data.getName(k) //liste des requètes numérotées
+    		  } 
+    		  
+    		  else 
+    		  {
+    		  listReponses ::= i + ") " + Data.getName(w) //liste des requètes numérotées
     		  i=i+1
     		  }
     		}
     		
-    		for(lr <- listReponses){
-    		  r.results ::= lr
-    		}
-    		r.results ::= "Quel est votre choix?"
+    		
     	}
     }
+    for(lr <- listReponses){
+    		  r.results ::= lr
+    		}
+    		
+    		r.results ::= "Quel est votre choix?"
   }
     
   def choose(r: Request): Unit = {
@@ -53,9 +70,10 @@ object Possibilities {
     }
     i=i+1
     }
-    r.results = List("L'adresse de " + Data.getName(listReponses(index).substring(3, listReponses(index).length())) +" est : "+ Data.getValue(r.results(index).substring(3, r.results(index).length())))
+    //r.results ::= "L'adresse de " + Data.getName(listReponses(index).substring(3, listReponses(index).length())) +" est : "+ Data.getValue(r.results(index).substring(3, r.results(index).length()))
                   
   }
+
   
 
 }
