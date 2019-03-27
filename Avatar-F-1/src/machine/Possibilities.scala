7085cbe97899b0ffe408455ee67b11aa786c7d89
xstun <xstun@148.60.138.45>
2019-03-27 17:55:13 +0100

    fin???

--- Avatar-F-1/src/machine/Possibilities.scala
+++ Avatar-F-1/src/machine/Possibilities.scala
@@ -1,44 +1,57 @@
 package machine
+import scala.util.control.Breaks
 
 object Possibilities {
     var plusieurs = false
     var nbReponses = 0
-  
+    var listReponses : List[String] = List()  
+    var responses: List[String] = List()
+    var L = List("je","une","de")
+    XMLSearch.init()
   def recherche(r :Request){
-   
-    var listReponses : List[String] = List()
     val words = r.rawInput.split(" ") 
-    val keys = Data.getKeys()
+    val keys = XMLSearch.getKeys()
     for(k <- keys) { //tous les mots clés de Data
-    	for(w <- words) { //tous les mots de la requète
-    		if(w.equals("un") || w.equals("une")){
-    			plusieurs = true
-    		}
-    		if(plusieurs){
+    	for(w <- words) { //tous les mots de la requète  
     			val keywords = k.split(" ")
-    			r.keywords = keywords.toList
-    			if(MyCorrection.distancedeHamming(k, w)){
+    			var j = 0
+    			var found = false
+    			while(j<keywords.length && !found) {
+    			//println(keywords(j))
+    			if(MyCorrection.distancedeHamming(w,keywords(j).toLowerCase()) && !L.contains(w)) {
+    			  println("WORD: "+w+ " AND " + keywords(j).toLowerCase())
     			  nbReponses +=1
-    			}
-    			
+    			  listReponses = k::listReponses
+    			  found=true
+    			  //println(nbReponses)
+    			  }
+    			j=j+1
+    			 }
     		}
+    }
+    println(nbReponses)
+    println(listReponses)
     		var i = 0
     		while(nbReponses != 0 && i<=nbReponses){
     		  if(i == 0){
+    		    responses ::= "J'ai " + nbReponses + " réponses possibles :"
     		    r.results ::= "J'ai " + nbReponses + " réponses possibles :"
     		    i=i+1
     		  } else {
-    		  listReponses ::= i + ") " +Data.getName(k) //liste des requètes numérotées
+    		    for(k<-listReponses) {
+    		  responses =  responses++ List((i + ") " +XMLSearch.getName(k))) //liste des requètes numérotées
     		  i=i+1
+    		    }
     		  }
     		}
-    		
-    		for(lr <- listReponses){
+    		responses = responses++List("Quel est votre choix?")
+    		responses.foreach(x => println(x))
+    		/*
+    		for(lr <- responses){
     		  r.results ::= lr
     		}
     		r.results ::= "Quel est votre choix?"
-    	}
-    }
+   */
   }
     
   def choose(r: Request): Unit = {
@@ -47,14 +60,15 @@ object Possibilities {
     var i = 1
     var index = 0
     while(i <= nbReponses && !choice) {
-    if (words.contains(i)) {
+    if (words.contains(i.toString())) {
       index = i
       choice = true
     }
     i=i+1
     }
-    r.results = List("L'adresse de " + Data.getName(r.results(index).substring(3, r.results(index).length())) +" est : "+ Data.getValue(r.results(index).substring(3, r.results(index).length())))
-                  
+   // println(index)
+    r.results = List("L'adresse de " + XMLSearch.getName(responses(index).substring(3, responses(index).length())) +" est : "+ XMLSearch.getValue(responses(index).substring(3, responses(index).length())))
+    println(r.results)              
   }
   
 
